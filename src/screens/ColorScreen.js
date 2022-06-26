eef5e95cd7bde3d500623edb5111f16a6259e56e
Mike R <or4ngedeuce@gmail.com>
2022-06-26 14:59:37 -0500

    Add state hook to ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,13 +1,42 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, FlatList } from "react-native";
 import CounterScreen from "./CounterScreen";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+  console.log(colors);
+
   return (
     <View>
       <Text>Color Screen Demo</Text>
+      <Button
+        title="Generate a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      ></Button>
+
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{ height: 100, width: 100, backgroundColor: item }}
+            ></View>
+          );
+        }}
+      ></FlatList>
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${green}, ${blue})`;
 };
 
 const styles = StyleSheet.create({});
