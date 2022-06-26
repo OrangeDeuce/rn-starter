ab43a496dcdac3177cbe8a41bf40f23b68ebb6c9
Mike R <or4ngedeuce@gmail.com>
2022-06-26 14:01:40 -0500

    Add ColorScreen component

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import CounterScreen from "./CounterScreen";
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Text>Color Screen Demo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
